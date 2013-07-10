8b953fab3d1b778eb5a044054c82bf7cd41700c6
Joakim George <johizzle@gmail.com>
2013-07-10 21:37:55 +0000

    testing readfilesync

--- web.js
+++ web.js
@@ -4,6 +4,8 @@ var fs = require('fs');
 var buffer = new Buffer(65555);
 var size = 0;
 
+var buffer = new Buffer(1024);
+
 fs.readFile('index.html','utf-8',function read(err,data){
     if(err){
 	return console.log(err);
@@ -13,11 +15,12 @@ fs.readFile('index.html','utf-8',function read(err,data){
     size = data.length;
 });
 
+var buffer2 = new Buffer(fs.readFileSync('index.html'),'utf-8');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buffer.toString());
+  response.send(buffer2.toString());
 });
 
 var port = process.env.PORT || 5000;
