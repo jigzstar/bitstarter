86beb8c5e0e4a8ee6abca9e10c421d37e53505f1
Joakim George <johizzle@gmail.com>
2013-07-10 13:48:55 +0000

    Update Web.js to read index.html

--- web.js
+++ web.js
@@ -1,9 +1,20 @@
 var express = require('express');
 
+var fs = require('fs');
+
+fs.readFile('index.html','utf-8',function read(err,data){
+    if(err){
+	return console.log(err);
+    }
+//   content = data;
+    var buffer = new Buffer(data,"utf-8");
+});
+
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
