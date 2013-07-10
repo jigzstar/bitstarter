cb2f2e16adf3b385a24d2de70a957985f5713a23
Joakim George <johizzle@gmail.com>
2013-07-10 14:04:13 +0000

    Updated tostring to use ascii

--- web.js
+++ web.js
@@ -15,7 +15,7 @@ fs.readFile('index.html','utf-8',function read(err,data){
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buffer.toString());
+  response.send(buffer.toString('ascii'));
 });
 
 var port = process.env.PORT || 5000;
